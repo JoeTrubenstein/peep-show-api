3806f68edaf3de2e2e6dc17727ef02029c912161
Joseph Trubenstein <josephtrubenstein@gmail.com>
2019-06-30 21:56:12 -0400

    debugging

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+from secrets import *
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1nb@i51y#qph4j539^n%er#(!yg*j5etn=8yjt_sv8u93=3s3$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ secrets.py
@@ -0,0 +1,3 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '1nb@i51y#qph4j539^n%er#(!yg*j5etn=8yjt_sv8u93=3s3$'
+DEBUG = True
